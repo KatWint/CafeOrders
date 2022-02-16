94f0fce5ac3aa3936f134d60e3cff7d3b55c9f8d
KatWint <92839131+KatWint@users.noreply.github.com>
2022-02-15 19:55:40 -0800

    finished product

--- CafeOrders/TestOrders.java
+++ CafeOrders/TestOrders.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class TestOrders {
     public static void main(String[] args) {
         
@@ -22,12 +24,42 @@ public class TestOrders {
 
         order1.name = "Cindhuri";
         order2.name = "Jimmy";
-        order3.name = "Sam";
+        order3.name = "Noah";
+        order4.name = "Sam";
 
+        order2.items.add(item1);
+        order2.total += item1.price;
+        
+        order3.items.add(item4);
+        order3.total += item4.price;
+        
+        order4.items.add(item2);
+        order4.total += item2.price;
 
+        order1.ready = true;
+        
+        order4.items.add(item2);
+        order4.total += item2.price;
+        //unsure how to add the item 2 times without redoing the code twice
+        order4.items.add(item2);
+        order4.total += item2.price;
+
+        order2.ready = true;
 
         System.out.printf("Name: %s\n", order1.name);
         System.out.printf("Total: %s\n", order1.total);
         System.out.printf("Ready: %s\n", order1.ready);
+
+        System.out.printf("Name: %s\n", order2.name);
+        System.out.printf("Total: %s\n", order2.total);
+        System.out.printf("Ready: %s\n", order2.ready);
+
+        System.out.printf("Name: %s\n", order3.name);
+        System.out.printf("Total: %s\n", order3.total);
+        System.out.printf("Ready: %s\n", order3.ready);
+
+        System.out.printf("Name: %s\n", order4.name);
+        System.out.printf("Total: %s\n", order4.total);
+        System.out.printf("Ready: %s\n", order4.ready);
     }
 }
