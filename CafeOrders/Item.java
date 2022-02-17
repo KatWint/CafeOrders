18aec4889805d59f16015c830c46eaf270d1ab5e
Kat Wint <wintkathryn@gmail.com>
2022-02-17 10:05:24 -0800

    first commit

--- CafeOrders/Item.java
+++ CafeOrders/Item.java
@@ -1,5 +1,29 @@
 public class Item {
-    public String name;
-    public double price;
+    private String name;
+    private double price;
+
+    public Item() {
+        
+    }
     
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice(double price) {
+        this.price = price;
+    }
+
+    public String setName(String name) {
+        this.name = name;
+    }
+
+    public double setPrice(double price) {
+        this.price = price;
+    }
 }
